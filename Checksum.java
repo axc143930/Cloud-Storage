4de35f3177aaa2675063a5b21fec509edc379e28
AKASH CHODAVARAPU <chodavarapu.akash@gmail.com>
2017-02-28 13:49:12 -0800

    Update Checksum.java

--- Checksum.java
+++ Checksum.java
@@ -5,21 +5,10 @@ import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- *
- * @author akash
- */
+
 public class Checksum {
 
-    /**
-     * @param args the command line arguments
-     * @return 
-     */
     public StringBuffer calculateChecksum(String datafile) throws NoSuchAlgorithmException, FileNotFoundException, IOException {
-        // TODO code application logic here
-        
-       // String datafile = "C:\\Users\\akash\\Desktop\\rucha.txt";
-
     MessageDigest md = MessageDigest.getInstance("SHA1");
     FileInputStream fis = new FileInputStream(datafile);
     byte[] dataBytes = new byte[1024];
@@ -37,8 +26,6 @@ public class Checksum {
     for (int i = 0; i < mdbytes.length; i++) {
     	sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
     }
-    
-    //System.out.println("Digest(in hex format):: " + sb.toString());
     return sb;
     
   }
